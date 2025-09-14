16a8e902b7c05f0fcfee1255595feb6a6442eca9
Ikit24 <szszattila2@gmail.com>
2025-09-14 13:04:29 +0300

    add: login command implemented

--- main.go
+++ main.go
@@ -3,11 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"github.com/Ikit24/aggreGATOR/internal/config"
 )
 
 type state struct {
 	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type handlerFunc func(*state, command) error
+
+type commands struct {
+	m map[string]handlerFunc
+}
+
+func (c *commands) register(name string, f handlerFunc) {
+	if c.m == nil {
+		c.m = make(map[string]handlerFunc)
+	}
+	c.m[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.m[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+	return handler(s, cmd)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("login expects a single argument, the username.")
+	}
+	username := cmd.args[0]
+	
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+	fmt.Println("user set to:", username)
+	return nil
 }
 
 func main() {
@@ -18,5 +58,18 @@ func main() {
 
 	s := &state{cfg: &cfgVal}
 
-	_ = s
+	cmds := &commands{m: make(map[string]handlerFunc)}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "not enough arguments")
+		os.Exit(1)
+	}
+	name := os.Args[1]
+	args := os.Args[2:]
+
+	if err := cmds.run(s, command{name: name, args: args}); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
