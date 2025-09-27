27b8e401c89964a1c7492cc5dd39eadad5c8da80
Ikit24 <szszattila2@gmail.com>
2025-09-27 19:00:20 +0300

    add: small progress on handlerFollows function

--- handler_follow.go
+++ handler_follow.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/Ikit24/aggreGATOR/internal/database"
 )
 
 func handlerFollow(s *state, cmd command) error {
@@ -16,4 +21,24 @@ func handlerFollow(s *state, cmd command) error {
 	}
 	url := cmd.Args[0]
 	
+	feed, err := s.db.GetFeedByURL(ctx, url)
+	if err != nil {
+		return err
+	}
+	now := time.Now().UTC()
+	id := uuid.New()
+
+	res, err := s.db.CreateFeedFollow(ctx, database.CreateFeedFollowParams{
+        ID:        id,
+        CreatedAt: now,
+        UpdatedAt: now,
+        UserID:    user.ID,
+        FeedID:    feed.ID,
+    })
+    if err != nil {
+        return err
+    }
+
+	fmt.Printf("%s %s\n", res.FeedName, res.UserName)
+	return nil
 }
