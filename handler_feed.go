20dae8dba74d24b125f7672d5c621dbc0b2ce4c5
Ikit24 <szszattila2@gmail.com>
2025-09-28 11:21:56 +0300

    finished follow and following command

--- handler_feed.go
+++ handler_feed.go
@@ -29,6 +29,18 @@ func handlerAddFeed(s *state, cmd command) error {
 	if err != nil {
 		return err
 	}
+
+	_, err = s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+	if err != nil {
+		return err
+	}
+
 	fmt.Printf("Feed created successfully!\n")
 	fmt.Printf("ID: %s\n", feed.ID)
 	fmt.Printf("Name: %s\n", feed.Name)
--- handler_follow.go
+++ handler_follow.go
@@ -6,29 +6,31 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+    "github.com/lib/pq"
 	"github.com/Ikit24/aggreGATOR/internal/database"
 )
 
 func handlerFollow(s *state, cmd command) error {
-	if len(cmd.Args) != 1 {
-		return fmt.Errorf("follow command requires one argument")
-	}
-	ctx := context.Background()
+    if len(cmd.Args) != 1 {
+        return fmt.Errorf("follow command requires one argument")
+    }
+    ctx := context.Background()
 
-	user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
-	if err != nil {
-		return err
-	}
-	url := cmd.Args[0]
-	
-	feed, err := s.db.GetFeedByURL(ctx, url)
-	if err != nil {
-		return err
-	}
-	now := time.Now().UTC()
-	id := uuid.New()
+    user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
+    if err != nil {
+        return err
+    }
+    url := cmd.Args[0]
 
-	res, err := s.db.CreateFeedFollow(ctx, database.CreateFeedFollowParams{
+    feed, err := s.db.GetFeedByURL(ctx, url)
+    if err != nil {
+        return err
+    }
+
+    now := time.Now().UTC()
+    id := uuid.New()
+
+    res, err := s.db.CreateFeedFollow(ctx, database.CreateFeedFollowParams{
         ID:        id,
         CreatedAt: now,
         UpdatedAt: now,
@@ -36,9 +38,32 @@ func handlerFollow(s *state, cmd command) error {
         FeedID:    feed.ID,
     })
     if err != nil {
+        if pqerr, ok := err.(*pq.Error); ok && pqerr.Code == "23505" {
+            fmt.Printf("%s %s\n", feed.Name, user.Name)
+            return nil
+        }
         return err
     }
 
 	fmt.Printf("%s %s\n", res.FeedName, res.UserName)
 	return nil
+}
+
+func handlerFollowing(s *state, cmd command) error {
+    ctx := context.Background()
+
+    user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
+    if err != nil {
+        return err
+    }
+
+    rows, err := s.db.GetFeedFollowsForUser(ctx, user.ID)
+    if err != nil {
+        return err
+    }
+
+    for _, r := range rows {
+        fmt.Println(r.FeedName)
+    }
+    return nil
 }
