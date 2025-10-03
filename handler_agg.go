5d28834b9446db67b17527f8b026e2684888a2fd
Ikit24 <szszattila2@gmail.com>
2025-10-03 19:14:18 +0300

    add: scrapeFeeds function

--- handler_agg.go
+++ handler_agg.go
@@ -14,3 +14,26 @@ func handlerAgg(s *state, cmd command) error {
 	fmt.Printf("%v+\n", feed)
 	return nil
 }
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err := s.db.MarkFeedFetched(ctx, feed.ID)
+	if err != nil {
+		return err
+	}
+
+	rssFeed, err := fetchFeed(ctx, feed.URL)
+	if err != nil {
+		return err
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("Title: %s\n", item.Title)
+	}
+	return nil
+}
