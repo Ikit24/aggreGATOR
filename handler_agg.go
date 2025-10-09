c0c8a9ab8f1045ed884e9e133a3f401a637dee35
Ikit24 <szszattila2@gmail.com>
2025-10-09 19:39:44 +0300

    add(feat): done posts, browse command works now

--- handler_agg.go
+++ handler_agg.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"time"
 	"strings"
+	"database/sql"
 
 	"github.com/Ikit24/aggreGATOR/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -50,10 +52,30 @@ func scrapeFeeds(s *state) error {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		t, ok := parsePubDate(item.PubDate)
+		desc := sql.NullString{
+			String: item.Description,
+			Valid: item.Description !="",
+		}
+
 		pub := sql.NullTime{}
-		if  ok {
+		if t, ok := parsePubDate(item.PubDate); ok {
 			pub = sql.NullTime{Time: t, Valid: true}
+		}
+
+		params := database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: desc,
+			PublishedAt: pub,
+			FeedID:      feed.ID,
+		}
+
+		if err := s.db.CreatePost(ctx, params); err	!= nil{
+			fmt.Println("create post error:", err)
+			continue
 		}
 	}
 	return nil
---
+++ handler_browse.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"strconv"
+
+	"github.com/Ikit24/aggreGATOR/internal/database"
+)
+
+func handlerBrowse(s *state, cmd command) error {
+	// 1) parse limit
+	limit := 2
+	if len(cmd.Args) == 1 {
+		n, err := strconv.Atoi(cmd.Args[0])
+		if err != nil || n <= 0 {
+			return fmt.Errorf("invalid limit")
+		}
+		limit = n
+	} else if len(cmd.Args) > 1 {
+		return fmt.Errorf("usage: %s [limit]", cmd.Name)
+	}
+
+	// 2) load current user (reuse your existing helper)
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return err
+	}
+
+	// 3) query posts
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  int32(limit),
+	})
+	if err != nil {
+		return err
+	}
+
+	// 4) print
+	for _, p := range posts {
+		pub := "unknown"
+		if p.PublishedAt.Valid {
+			pub = p.PublishedAt.Time.Format(time.RFC3339)
+		}
+		fmt.Printf("%s\n%s\npublished: %s\n\n", p.Title, p.Url, pub)
+	}
+	return nil
+}
